748b2ef9388386a8a62d6c4ef4e5f5c69f2940b2
Mark Simmons <marks21@utexas.edu>
2023-07-25 19:40:39 -0500

    put script in main function to enable entrypoint as script in pyproject.toml

--- src/rVAD/rVAD_fast.py
+++ src/rVAD/rVAD_fast.py
@@ -19,51 +19,55 @@ from copy import deepcopy
 
 # Usage: python rVAD_fast_2.0.py inWaveFile  outputVadLabel
 
-# MODIFICATION BY Mark Simmons 7/25/2023
-# Indicate original copyright upon execution
-print("rVAD fast 2.0: Copyright (c) 2022 Zheng-Hua Tan and Achintya Kumar Sarkar")
-# END MODIFICATION
+def main():
+    # MODIFICATION BY Mark Simmons 7/25/2023
+    # Indicate original copyright upon execution
+    print("rVAD fast 2.0: Copyright (c) 2022 Zheng-Hua Tan and Achintya Kumar Sarkar")
+    # END MODIFICATION
 
-winlen, ovrlen, pre_coef, nfilter, nftt = 0.025, 0.01, 0.97, 20, 512
-ftThres=0.5; vadThres=0.4
-opts=1
+    winlen, ovrlen, pre_coef, nfilter, nftt = 0.025, 0.01, 0.97, 20, 512
+    ftThres=0.5; vadThres=0.4
+    opts=1
 
-finwav=str(sys.argv[1])
-fvad=str(sys.argv[2])
+    finwav=str(sys.argv[1])
+    fvad=str(sys.argv[2])
 
-fs, data = speechproc.speech_wave(finwav)   
-ft, flen, fsh10, nfr10 =speechproc.sflux(data, fs, winlen, ovrlen, nftt)
-
-
-# --spectral flatness --
-pv01=numpy.zeros(nfr10)
-pv01[numpy.less_equal(ft, ftThres)]=1 
-pitch=deepcopy(ft)
-
-pvblk=speechproc.pitchblockdetect(pv01, pitch, nfr10, opts)
+    fs, data = speechproc.speech_wave(finwav)   
+    ft, flen, fsh10, nfr10 =speechproc.sflux(data, fs, winlen, ovrlen, nftt)
 
 
-# --filtering--
-ENERGYFLOOR = numpy.exp(-50)
-b=numpy.array([0.9770,   -0.9770])
-a=numpy.array([1.0000,   -0.9540])
-fdata=lfilter(b, a, data, axis=0)
+    # --spectral flatness --
+    pv01=numpy.zeros(nfr10)
+    pv01[numpy.less_equal(ft, ftThres)]=1 
+    pitch=deepcopy(ft)
+
+    pvblk=speechproc.pitchblockdetect(pv01, pitch, nfr10, opts)
 
 
-#--pass 1--
-noise_samp, noise_seg, n_noise_samp=speechproc.snre_highenergy(fdata, nfr10, flen, fsh10, ENERGYFLOOR, pv01, pvblk)
+    # --filtering--
+    ENERGYFLOOR = numpy.exp(-50)
+    b=numpy.array([0.9770,   -0.9770])
+    a=numpy.array([1.0000,   -0.9540])
+    fdata=lfilter(b, a, data, axis=0)
 
-#sets noisy segments to zero
-for j in range(n_noise_samp):
-    fdata[range(int(noise_samp[j,0]),  int(noise_samp[j,1]) +1)] = 0 
+
+    #--pass 1--
+    noise_samp, noise_seg, n_noise_samp=speechproc.snre_highenergy(fdata, nfr10, flen, fsh10, ENERGYFLOOR, pv01, pvblk)
+
+    #sets noisy segments to zero
+    for j in range(n_noise_samp):
+        fdata[range(int(noise_samp[j,0]),  int(noise_samp[j,1]) +1)] = 0 
 
 
-vad_seg=speechproc.snre_vad(fdata,  nfr10, flen, fsh10, ENERGYFLOOR, pv01, pvblk, vadThres)
+    vad_seg=speechproc.snre_vad(fdata,  nfr10, flen, fsh10, ENERGYFLOOR, pv01, pvblk, vadThres)
 
-numpy.savetxt(fvad, vad_seg.astype(int),  fmt='%i')
-print("%s --> %s " %(finwav, fvad))
+    numpy.savetxt(fvad, vad_seg.astype(int),  fmt='%i')
+    print("%s --> %s " %(finwav, fvad))
 
-data=None; pv01=None; pitch=None; fdata=None; pvblk=None; vad_seg=None
+    data=None; pv01=None; pitch=None; fdata=None; pvblk=None; vad_seg=None
+
+if __name__ == '__main__':
+    main()
      
 
 
