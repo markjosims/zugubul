a61145abb9fd01d64f9e53ee094b39a8f6735f95
Mark Simmons <marks21@utexas.edu>
2023-08-14 13:19:57 -0500

    create and test functionality for initializing a HF audiofolder dataset

--- src/zugubul/models/dataset.py
+++ src/zugubul/models/dataset.py
@@ -1,102 +1,25 @@
-import csv
-from pathlib import Path
-from dataclasses import dataclass
-from typing import Optional
+from typing import Union
+import os
 
-import torch
-import torch.utils.data as data
-from torch.nn.utils.rnn import pad_sequence
+from datasets import Dataset, load_dataset
 
-from datasets import Wav2Vec2CTCTokenizer
-
-from zugubul.models._constants import CHARACTER_TYPE
-from zugubul.models._constants import FEATURE_TYPE
-from zugubul.models._constants import SPECIAL_TOKEN_TYPE
-from zugubul.models.models import LangIdentifierInput
-# from zugubul.models.vocab import encode
-# from zugubul.models.vocab import END_TOKEN
-# from zugubul.models.vocab import END_TOKEN_IDX
-# from zugubul.models.vocab import PAD_TOKEN_IDX
-# from zugubul.models.vocab import START_TOKEN
-# from zugubul.models.vocab import START_TOKEN_IDX
-
-@dataclass
-class RawDatum:
+def init_lid_dataset(dirpath: Union[str, os.PathLike]) -> Dataset:
     """
-    The unprocessed features of single training example
-
-    Attributes
-    ----------
-    text: The transcription or label to be predicted
-    audio_fp: Path to recording
-    start: Start time (in ms) for interval in recording (default 0)
-    end: End time (in ms) for interval in recording (default -1, i.e. end of recording)
+    dirpath indicates dataset directory
+    directory must be of following structure:
+    dirpath
+        metadata.csv
+        train
+            recording1.wav
+            recording2.wav
+            ...
+        val
+            recording3.wav
+            recording4.wav
+            ...
+        test
+            recording5.wav
+            recording6.wav
+            ...
     """
-    text: str
-    audio_fp: str
-    start: int = 0
-    end: int = -1
-
-
-
-class Dataset(data.Dataset):
-    def __init__(self, path: Path, tokenizer: Wav2Vec2CTCTokenizer) -> None:
-        with open(path) as csvfile:
-            r = csv.reader(csvfile, delimiter="\t")
-            self.data = [RawDatum(*line) for line in r]
-
-    def __getitem__(self, index: int) -> LangIdentifierInput:
-        ...
-        text_tokens = torch.tensor(encode(self.data[index].text, "text"))
-        lemma_tokens = torch.tensor(encode(self.data[index].audio_fp, "audio_fp"))
-
-        # src_tokens = torch.cat(
-        #     [
-        #         torch.tensor([START_TOKEN_IDX]),
-        #         feature_tokens,
-        #         lemma_tokens,
-        #         torch.tensor([END_TOKEN_IDX]),
-        #     ]
-        # )
-        # src_types = torch.tensor(
-        #     [SPECIAL_TOKEN_TYPE]
-        #     + [FEATURE_TYPE for _ in range(len(feature_tokens))]
-        #     + [CHARACTER_TYPE for _ in range(len(lemma_tokens))]
-        #     + [SPECIAL_TOKEN_TYPE]
-        # )
-        # tgt_tokens = torch.tensor(
-        #     encode(START_TOKEN + self.data[index].word + END_TOKEN, "characters")
-        # )
-        # tgt_tokens_in = tgt_tokens[:-1]
-        # tgt_tokens_out = tgt_tokens[1:]
-        # tgt_mask = torch.nn.Transformer.generate_square_subsequent_mask(
-        #     tgt_tokens_in.size(0)
-        # )
-        # return LangIdentifierInput(
-        #     src_tokens, src_types, tgt_tokens_in, tgt_tokens_out, tgt_mask
-        # )
-
-    def __len__(self) -> int:
-        return len(self.data)
-
-
-# def collate_fn(batch: Sequence[LangIdentifierInput]) -> LangIdentifierInput:
-#     src_tokens = pad_sequence(
-#         [item.src_tokens for item in batch], padding_value=PAD_TOKEN_IDX
-#     )
-#     src_types = pad_sequence(
-#         [item.src_types for item in batch], padding_value=SPECIAL_TOKEN_TYPE
-#     )
-#     tgt_tokens_in = pad_sequence(
-#         [item.tgt_tokens_in for item in batch], padding_value=PAD_TOKEN_IDX
-#     )
-#     tgt_tokens_out = pad_sequence(
-#         [item.tgt_tokens_out for item in batch], padding_value=PAD_TOKEN_IDX
-#     )
-#     tgt_mask = torch.nn.Transformer.generate_square_subsequent_mask(
-#         tgt_tokens_in.size(0)
-#     )
-
-#     return LangIdentifierInput(
-#         src_tokens, src_types, tgt_tokens_in, tgt_tokens_out, tgt_mask
-#     )
+    return load_dataset('audiofolder', data_dir=dirpath)
--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -1,15 +1,40 @@
-from zugubul.models.dataset import RawDatum, Dataset
+from zugubul.models.dataset import init_lid_dataset
+import pytest
+import csv
+import os
+import shutil
+import tempfile
 
-def test_raw_datum1():
-    datum = RawDatum('potato', 'potato.wav')
-    assert datum.text == 'potato'
-    assert datum.audio_fp == 'potato.wav'
-    assert datum.start == 0
-    assert datum.end == -1
+@pytest.fixture
+def tmp_dataset():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        dir_path = os.path.join(tmpdir, 'dataset')
+        data_dir = os.path.join(dir_path, 'data')
+        train_dir = os.path.join(data_dir, 'train')
+        test_dir = os.path.join(data_dir, 'test')
+        val_dir = os.path.join(data_dir, 'val')
 
-def test_raw_datum2():
-    datum = RawDatum('potato', 'potato.wav', 10, 30)
-    assert datum.text == 'potato'
-    assert datum.audio_fp == 'potato.wav'
-    assert datum.start == 10
-    assert datum.end == 30
+        os.mkdir(dir_path)
+        os.mkdir(data_dir)
+        os.mkdir(train_dir)
+        os.mkdir(test_dir)
+        os.mkdir(val_dir)
+
+        shutil.copy(r'C:\projects\zugubul\tests\wavs\train\train.wav', os.path.join(train_dir, 'train.wav'))
+        shutil.copy(r'C:\projects\zugubul\tests\wavs\test\test.wav', os.path.join(test_dir, 'test.wav'))
+        shutil.copy(r'C:\projects\zugubul\tests\wavs\val\val.wav', os.path.join(val_dir, 'val.wav'))
+        csv_path = os.path.join(dir_path, 'metadata.csv')
+        with open(csv_path, 'w') as f:
+            writer = csv.writer(f, delimiter=',')
+            rows = [
+                ['file_name', 'text'],
+                ['data/train/train.wav', 'train'],
+                ['data/test/test.wav', 'test'],
+                ['data/val/val.wav', 'val'],
+            ]
+            writer.writerows(rows)
+        yield dir_path
+
+def test_init_lid_dataset(tmp_dataset):
+    dataset = init_lid_dataset(tmp_dataset)
+    assert dataset['train'][0]['text'] == 'train'
