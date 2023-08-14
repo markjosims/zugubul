338eb9f2ba568cd8cd3fa51011808927dd871c7d
Mark Simmons <marks21@utexas.edu>
2023-08-14 13:21:54 -0500

    add fixture for making temporary data csv

--- tests/vocab_test.py
+++ tests/vocab_test.py
@@ -1,8 +1,27 @@
+import tempfile
+import pytest
 import csv
 import json
 import os
 
-from zugubul.models.vocab import tokenizer_from_csv, tokenizer_from_list, vocab_from_list
+from zugubul.models.vocab import tokenizer_from_csv, tokenizer_from_list, vocab_from_list, init_processor
+
+@pytest.fixture
+def tmp_csv():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        vocab_dir = os.path.join(tmpdir, 'tmp_vocab')
+        os.mkdir(vocab_dir)
+        csv_path = os.path.join(vocab_dir, 'vocab.csv')
+        csv_vocab = [
+            ['[ENG]', 'foo.wav', 0, 1],
+            ['[TIC]', 'foo.wav', 2, 3],
+            ['[DDN]', 'bar.wav', 0, 1],
+            ['[ENG]', 'bar.wav', 4, 5],
+        ]
+        with open(csv_path, 'w') as f:
+            vocab_writer = csv.writer(f, delimiter='\t', dialect='unix')
+            vocab_writer.writerows(csv_vocab)
+        yield csv_path, vocab_dir
 
 def test_vocab_from_list(tmp_path):
     vocab_dir = tmp_path / 'vocab1'
@@ -34,19 +53,8 @@ def test_tokenizer_from_list(tmp_path):
     assert tokenizer.encode('[TIC][ENG][ENG][DDN][TIC]') == [1, 0, 0, 2, 1]
 
 
-def test_tokenizer_from_csv(tmp_path):
-    vocab_dir = tmp_path / 'vocab3'
-    vocab_dir.mkdir()
-    csv_path = os.path.join(vocab_dir, 'vocab.csv')
-    csv_vocab = [
-        ['[ENG]', 'foo.wav', 0, 1],
-        ['[TIC]', 'foo.wav', 2, 3],
-        ['[DDN]', 'bar.wav', 0, 1],
-        ['[ENG]', 'bar.wav', 4, 5],
-    ]
-    with open(csv_path, 'w') as f:
-        vocab_writer = csv.writer(f, delimiter='\t', dialect='unix')
-        vocab_writer.writerows(csv_vocab)
+def test_tokenizer_from_csv(tmp_csv):
+    csv_path, vocab_dir = tmp_csv
     tokenizer = tokenizer_from_csv(csv_path=csv_path, vocab_dir=vocab_dir)
     # because tokenizer_from_csv puts the token in a set, we can't know for certain what the indices will be
     # for that reason we're not checking the encoded values here
@@ -58,9 +66,20 @@ def test_special_tokens(tmp_path):
         '[TIC]',
         '[DDN]',
     ]
-    vocab_dir = tmp_path / 'vocab2'
+    vocab_dir = tmp_path / 'vocab4'
     vocab_dir.mkdir()
     tokenizer = tokenizer_from_list(vocab=vocab, vocab_dir=vocab_dir)
     assert tokenizer.pad_token == '<pad>'
     assert tokenizer.unk_token == '<unk>'
     assert tokenizer.word_delimiter_token == '|'
+
+def test_init_processor(tmp_path):
+    vocab = [
+        '[ENG]',
+        '[TIC]',
+        '[DDN]',
+    ]
+    vocab_dir = tmp_path / 'vocab5'
+    vocab_dir.mkdir()
+    processor = init_processor(vocab=vocab, vocab_dir=vocab_dir)
+    assert processor(text='[ENG][TIC][DDN]').input_ids == [0, 1, 2]
