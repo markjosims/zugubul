dbc36ea7384c38d607cb75307caffe3269250cc2
Mark Simmons <mjsimmons@ucsd.edu>
2023-09-12 10:28:32 -0700

    implement test_infer_lid

---
+++ tests/infer_test.py
@@ -0,0 +1,14 @@
+from zugubul.models.infer import infer_lid
+
+from pympi import Elan
+
+def test_infer_lid():
+    wav_fp = r'C:\projects\zugubul\tests\wavs\test\test.wav'
+    model_path = 'markjosims/wav2vec2-large-mms-1b-tira-lid'
+
+    eaf = infer_lid(wav_fp, model_path)
+
+    annotations = eaf.get_annotation_data_for_tier('default-lt')
+
+    for a in annotations:
+        assert a[0] in ('ENG', 'TIC')
