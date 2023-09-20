f1dc6103fa467cab66a89b2ecdfdb60fd076468f
Mark Simmons <marks21@utexas.edu>
2023-09-20 14:48:14 -0700

    fixed typo in inference_method argname

--- src/zugubul/remote.py
+++ src/zugubul/remote.py
@@ -3,7 +3,7 @@
 import os
 from fabric import Connection
 from paramiko.ssh_exception import PasswordRequiredException
-from typing import Sequence
+from typing import Sequence, List
 from pathlib import Path
 from time import time
 
@@ -16,9 +16,11 @@ def run_script_on_server(
     ) -> int:
     with connect(server) as c:
         # replace local fps w server fps in arg str and put to server
+        # TODO: dynamically check if input file is already present
         print(f'Uploading input files to server {server}...')
-        server_dir = Path(f'/tmp/annotate_{time():.0f}')
-        c.run(f'mkdir {server_dir}')
+        #server_dir = Path(f'/tmp/annotate_{time():.0f}/')
+        server_dir = Path('/tmp/annotate_1695240969/')
+        #c.run(f'mkdir {server_dir}')
         for local_fp in in_files:
             filename = Path(local_fp).name
             server_fp = server_dir/filename
@@ -37,12 +39,25 @@ def run_script_on_server(
 
         print('Executing command on server...')
         argv = [server_python, '-m', 'zugubul.main'] + argv[1:]
-        c.run(' '.join(argv))
+        arg_str = make_arg_str(argv)
+        c.run(arg_str)
 
         print('Downloading output files from server...')
         for local_fp, server_fp in zip(out_files, server_out_files):
             c.get(str(server_fp), str(local_fp))
             print('Output filed saved to', local_fp)
+
+def make_arg_str(argv: Sequence[str]) -> str:
+    """
+    Wrap any arguments broken by whitespace with quotes,
+    then join arguments into str and return.
+    """
+    has_whitespace = lambda s: any(c.isspace() for c in s)
+    wrap_if_whitespace = lambda s: '"'+s+'"' if has_whitespace(s) else s
+    argv = [wrap_if_whitespace(s) for s in argv]
+    arg_str = ' '.join(argv)
+    return arg_str
+
 
 def connect(address: str) -> Connection:
     connect_kwargs = {
@@ -74,7 +89,7 @@ if __name__ == '__main__':
             'markjosims/wav2vec2-large-xls-r-300m-tira-colab',
             'TIC',
             eaf_file,
-            '--inference method', 'local',
+            '--inference_method', 'local',
             '-t', 'IPA Transcription',
             etf_file,
         ],
