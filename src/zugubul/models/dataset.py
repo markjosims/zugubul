b29455b57fa4852f95af7670d8f21c5588ee5869
Mark Simmons <marks21@utexas.edu>
2023-11-21 20:40:18 -0800

    make_lm_datasets returns train, val, test splits

--- src/zugubul/models/dataset.py
+++ src/zugubul/models/dataset.py
@@ -1,7 +1,7 @@
 from typing import Union, Sequence, Tuple, Optional, Literal
 import os
 import pandas as pd
-from datasets import Dataset, load_dataset
+from datasets import Dataset, DatasetDict, load_dataset
 from huggingface_hub import login
 from zugubul.elan_tools import snip_audio, eaf_data
 from math import ceil
@@ -90,10 +90,11 @@ def split_data(
 
 def make_lm_dataset(
         annotations: Union[str, pd.DataFrame],
-) -> str:
+        splitsize: Tuple[float, float, float] = (0.8, 0.1, 0.1)
+) -> pd.DataFrame:
     """
     Concatenates all text from a dataframe or .csv file containing ASR labels
-    and creates a textfile to be used for LM training.
+    and returns a dataframe to be used for LM training.
     Assumes that all language labels have been trimmed,
     meaning you should run make_asr_labels on your .eaf files before this.
     """
@@ -103,8 +104,20 @@ def make_lm_dataset(
         annotations = pd.read_csv(annotations)
     add_period = lambda s: s+'.' if s.strip()[-1] not in string.punctuation else s.strip()
 
-    out = ' '.join(annotations['text'].apply(add_period))
-    return out
+    text = ' '.join(annotations['text'].apply(add_period))
+    train_size, val_size, _ = splitsize
+    train_chars = int(len(text)*train_size)
+    val_chars = train_chars+int(len(text)*val_size)
+    train = Dataset.from_dict({'text': text[:train_chars]})
+    val = Dataset.from_dict({'text': text[train_chars:val_chars]})
+    test = Dataset.from_dict({'text': text[val_chars:]})
+    
+    return DatasetDict({
+        'train': train,
+        'val': val,
+        'test': test
+    })
+
 
 def make_asr_labels(
         annotations: Union[str, pd.DataFrame],
