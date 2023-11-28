7131e4feb915a5fb039dc55061352433f898a42e
Mark Simmons <marks21@utexas.edu>
2023-11-28 08:09:30 -0800

    compute_str methods accept tensors or strings

--- src/zugubul/models/_metrics.py
+++ src/zugubul/models/_metrics.py
@@ -1,28 +1,39 @@
 from evaluate import load, EvaluationModule
+from transformers import PreTrainedTokenizer
 import numpy as np
-from typing import Dict
+from typing import Dict, Union, Mapping, Optional
+import torch
 
 wer_metric = load("wer")
 cer_metric = load("cer")
 accuracy = load("accuracy")
 
-def compute_str_acc(
-        pred,
-        processor,
-        metrics: Dict[str, EvaluationModule],
-    ):
-    pred_logits = pred.predictions
+def get_pred_str(pred_logits, processor) -> str:
     pred_ids = np.argmax(pred_logits, axis=-1)
-
-    if hasattr(processor, 'pad_token_id'):
-        pad_token_id = processor.pad_token_id
-    else:
-        pad_token_id = processor.tokenizer.pad_token_id
-    pred.label_ids[pred.label_ids == -100] = pad_token_id
-
     pred_str = processor.batch_decode(pred_ids)
-    # we do not want to group tokens when computing the metrics
-    label_str = processor.batch_decode(pred.label_ids, group_tokens=False)
+    return pred_str    
+
+def compute_str_acc(
+        pred: Union[Mapping, str],
+        processor: Optional[PreTrainedTokenizer] = None,
+        pred_logits: Optional[torch.tensor] = None,
+        label_str: Optional[str] = None,
+        metrics: Dict[str, EvaluationModule] = 'wer',
+        return_pred_str: bool = False,
+    ):
+    if type(pred) is not str:
+        if not pred_logits:
+            pred_logits = pred.predictions
+        pred_str = get_pred_str(pred_logits, processor)
+
+    if label_str is None:
+        if hasattr(processor, 'pad_token_id'):
+            pad_token_id = processor.pad_token_id
+        else:
+            pad_token_id = processor.tokenizer.pad_token_id
+        pred.label_ids[pred.label_ids == -100] = pad_token_id
+        # we do not want to group tokens when computing the metrics
+        label_str = processor.batch_decode(pred.label_ids, group_tokens=False)
 
     metric_outs = dict()
     for metric_name, metric_funct in metrics.items():
@@ -30,17 +41,19 @@ def compute_str_acc(
             predictions=pred_str,
             references=label_str
         )
+    if return_pred_str:
+        metric_outs['pred'] = pred_str
 
     return metric_outs
 
-def compute_wer(pred, processor):
-    return compute_str_acc(pred, processor, metrics={'wer': wer_metric})
+def compute_wer(*args, **kwargs):
+    return compute_str_acc(*args, **kwargs, metrics={'wer': wer_metric})
 
-def compute_cer(pred, processor):
-    return compute_str_acc(pred, processor, metrics={'cer': cer_metric})
+def compute_cer(*args, **kwargs):
+    return compute_str_acc(*args, **kwargs, metrics={'cer': cer_metric})
 
-def compute_cer_and_wer(pred, processor):
-    return compute_str_acc(pred, processor, metrics={'wer': wer_metric, 'cer': cer_metric})
+def compute_cer_and_wer(*args, **kwargs):
+    return compute_str_acc(*args, **kwargs, metrics={'wer': wer_metric, 'cer': cer_metric})
 
 # taken from https://huggingface.co/docs/transformers/tasks/audio_classification on Sep 12 2023
 def compute_acc(pred):
