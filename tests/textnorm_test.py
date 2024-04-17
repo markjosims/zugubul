fa7062c5639a91660593bf9fd6b0d9410e10ae6c
Mark Simmons <marks21@utexas.edu>
2024-04-16 17:19:29 -0700

    implement tone normalization functs

--- tests/textnorm_test.py
+++ tests/textnorm_test.py
@@ -2,6 +2,9 @@ from zugubul.textnorm import (
     unicode_normalize,
     make_replacements,
     remove_punct,
+    strip_diacs,
+    get_tone_as_letters,
+    split_segs_and_tone,
     COMBINING
 )
 
@@ -39,4 +42,22 @@ def test_remove_punct():
     text = 'hi?!?@#$^? ho;<>?[\{\}]w a%*()re you?\'"'
     gold = 'hi how are you'
     pred = remove_punct(text)
+    assert pred == gold
+
+def test_strip_tone():
+    text = 'tóňàl ẁōr̂d'
+    gold = 'tonal word'
+    pred = strip_diacs(text)
+    assert pred == gold
+
+def test_get_tone_as_letters():
+    text = 'tóňàl ẁōr̂d'
+    gold = 'H-LH-L L-M-HL'
+    pred = get_tone_as_letters(text)
+    assert pred == gold
+
+def test_split_segs_and_tone():
+    text = 'tóňàl ẁōr̂d'
+    gold = ('tonal word', 'H-LH-L L-M-HL')
+    pred = split_segs_and_tone(text)
     assert pred == gold
